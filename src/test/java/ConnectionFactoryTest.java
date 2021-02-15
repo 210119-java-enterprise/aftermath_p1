1e1fd8446e869114331ed78f1375ac1729f31003
Jay Norman <jonathan.norman.park.alumni@gmail.com>
2021-02-14 20:28:24 -0600

    Added unit tests for Connection Factory class

---
+++ src/test/java/ConnectionFactoryTest.java
@@ -0,0 +1,31 @@
+import com.revature.utils.ConnectionFactory;
+import org.junit.After;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Properties;
+
+import static org.junit.Assert.*;
+
+public class ConnectionFactoryTest {
+    @Test
+    public void connectionFactoryCreatesOnlyOneFactoryInstance() {
+        ConnectionFactory c1 = ConnectionFactory.getInstance();
+        ConnectionFactory c2 = ConnectionFactory.getInstance();
+        assertSame(c1, c2);
+    }
+
+    @Test
+    public void connectionFactoryCreatesOnlyOneConnectionInstance() throws IOException {
+        Properties props = new Properties();
+        props.load(new FileReader("src/main/resources/application.properties"));
+        ConnectionFactory.addCredentials(props);
+        Connection conn1 = ConnectionFactory.getInstance().getConnection();
+        Connection conn2 = ConnectionFactory.getInstance().getConnection();
+
+        assertSame(conn1, conn2);
+    }
+}
